f185fbe821dd81bc4d60719429764a6319344c4a
Maxim <kmd@tigrio.ru>
2018-03-06 18:57:54 +0700

    background to header sections added

--- src/components/main-header/script.js
+++ src/components/main-header/script.js
@@ -5,11 +5,74 @@ export default {
 
   data: function () {
     return {
-      activePoint: false
+      mainChecked: false,
+      shuffleChecked: false,
+      investingChecked: false,
+      howWorkChecked: false,
+      faqChecked: false,
+      feesChecked: false,
+      name: ''
     }
   },
 
   methods: {
-
+    switchBlock(blockName) {
+      this.mainChecked = false;
+      this.shuffleChecked = false;
+      this.investingChecked = false;
+      this.howWorkChecked = false;
+      this.faqChecked = false;
+      this.feesChecked =  false;
+      switch(blockName) {
+        case 'main':
+          this.mainChecked = true;
+          this.shuffleChecked = false;
+          this.investingChecked = false;
+          this.howWorkChecked = false;
+          this.faqChecked = false;
+          this.feesChecked =  false;
+          break;
+        case 'shuffle':
+          this.mainChecked = false;
+          this.shuffleChecked = true;
+          this.investingChecked = false;
+          this.howWorkChecked = false;
+          this.faqChecked = false;
+          this.feesChecked =  false;
+          break;
+        case 'invest':
+          this.mainChecked = false;
+          this.shuffleChecked = false;
+          this.investingChecked = true;
+          this.howWorkChecked = false;
+          this.faqChecked = false;
+          this.feesChecked =  false;
+          break;
+        case 'howWork':
+          this.mainChecked = false;
+          this.shuffleChecked = false;
+          this.investingChecked = false;
+          this.howWorkChecked = true;
+          this.faqChecked = false;
+          this.feesChecked =  false;
+          break;
+        case 'faq':
+          this.mainChecked = false;
+          this.shuffleChecked = false;
+          this.investingChecked = false;
+          this.howWorkChecked = false;
+          this.faqChecked = true;
+          this.feesChecked =  false;
+          break;
+        case 'fees':
+          this.mainChecked = false;
+          this.shuffleChecked = false;
+          this.investingChecked = false;
+          this.howWorkChecked = false;
+          this.faqChecked = false;
+          this.feesChecked =  true;
+          break;
+      }
+    }
   }
 }
