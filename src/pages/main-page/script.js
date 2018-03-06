b241cb70807294c32fa0649b8a2a237392d3a017
Oksana Kritskaya <ovk@tigrio.ru>
2018-03-06 13:11:15 +0700

    fonts

--- src/pages/main-page/script.js
+++ src/pages/main-page/script.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import mainHeader from '../../components/main-header/main-header'
+import mainIndex from '../../components/main-index/main-index'
+import mainContent from '../../components/main-content/main-content'
 
 export default {
   name: 'main-page',
   components: {
-    mainHeader
+    mainHeader,
+    mainIndex,
+    mainContent
   }
 }
