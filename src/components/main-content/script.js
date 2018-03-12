e5cf961acf653badd50da2cd49e610239e57d832
Oksana Kritskaya <ovk@tigrio.ru>
2018-03-12 15:25:10 +0700

    page main - add click on button

--- src/components/main-content/script.js
+++ src/components/main-content/script.js
@@ -1,3 +1,17 @@
 export default {
 	name: 'main-content',
+	data: function() {
+    return {
+			activePart: true,
+			activeRead: false
+		}
+		
+  },
+
+	methods: {
+		changeIndex: function() {
+			this.activePart = !this.activePart;
+			this.activeRead = !this.activeRead;
+    }
+  }
 };
--- src/components/main-index/script.js
+++ src/components/main-index/script.js
@@ -1,3 +1,18 @@
 export default {
 	name: 'main-index',
+
+	data: function() {
+    return {
+			activeShuffle: true,
+			activeMore: false
+		}
+		
+  },
+
+	methods: {
+		changeIndex: function() {
+			this.activeShuffle = !this.activeShuffle;
+			this.activeMore = !this.activeMore;
+    }
+  }
 };
